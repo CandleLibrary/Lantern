75ee37b532d184296e21b48bbca8847f687f0306
galactrax <anthonycweathersby@gmail.com>
2019-02-25 22:12:19 -0700

    incremented patch version

--- source/default_dispatch.mjs
+++ source/default_dispatch.mjs
@@ -1,7 +1,7 @@
 const default_dispatch = {
 	response_type: 0,
     name: "Default",
-    respond:  async () => {return false}
+    respond:  async (tools) => {return false}
 }
 
 export default default_dispatch;
