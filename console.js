257b1a9e7374bc6329a39b2983aa9f2416e18f63
galactrax <anthonycweathersby@gmail.com>
2019-05-14 17:58:57 -0600

    correcting mistake with the shebang

--- console.js
+++ console.js
@@ -1,15 +1,14 @@
-#!/bin/sh node 
+#!/usr/bin/env node
 
 const { spawn } = require('child_process');
+
 const path = require("path")
 
-let runner = spawn("node", ["--experimental-modules", path.resolve(require.main.path, "cli.mjs")]);
+let runner = spawn("node", ["--experimental-modules", path.resolve(".", "cli.mjs")]);
 
 runner.stdout.pipe(process.stdout);
 
 runner.stderr.pipe(process.stderr);
-
-process.stdin.pipe(runner.stdin);
 
 runner.on('close', (code) => {
   console.log(`child process exited with code ${code}`);
