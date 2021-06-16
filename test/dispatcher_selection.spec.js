9548c7d6b822be7e7d692f8c12554175151b72fc
Anthony Weathersby <acweathersby.codes@gmail.com>
2021-06-16 17:51:49 -0600

    Chore: Update tests

--- test/dispatcher_selection.spec.js
+++ test/dispatcher_selection.spec.js
@@ -5,15 +5,20 @@
  * ASCENDING based on the order in which the dispatchers
  * where submitted to AddDispatch
  */
-import { AddDispatch, getDispatches } from "@candlelib/lantern/build/library/dispatcher.js";
-import ext_map from "@candlelib/lantern/build/library/extension_map.js";
+import { AddDispatch, getDispatches } from "@candlelib/lantern/build/library/dispatchers/dispatch.js";
+import { LogQueue } from "@candlelib/lantern/build/library/utils/log.js";
+import ext_map from "@candlelib/lantern/build/library/extension/extension_map.js";
 import URL from "@candlelib/uri";
+
+await URL.server();
+
+const logger = new LogQueue(console.log);
 
 const
     DispatchMap = new Map(),
     DispatchDefaultMap = new Map();
 
-AddDispatch(DispatchMap, DispatchDefaultMap,
+AddDispatch(logger, DispatchMap, DispatchDefaultMap,
     {
         name: "Images",
         respond: _ => _,
@@ -42,10 +47,8 @@ function getDispatchName(url) {
     return (getDispatches({ url }, DispatchMap, ext_map) ?? []).map(d => d.name);
 }
 
-"/img/test.png";
-assert(getDispatchName(urlA) == ['Images', 'AllImages', 'LastResort']);
+assert("/img/test.png", getDispatchName(urlA) == ['Images', 'AllImages', 'LastResort']);
 
 urlA = new URL("http://localhost/img/png/test.png");
 
-"/img/png/test.png";
-assert(getDispatchName(urlA) == ['Images', 'AllImages', 'LastResort']);
+assert("/img/png/test.png", getDispatchName(urlA) == ['Images', 'AllImages', 'LastResort']);
